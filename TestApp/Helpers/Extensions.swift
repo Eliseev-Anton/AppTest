016d03c3b45bd6197252bc0459c92b19226b2227
Anton <eliseev2006.anton@gmail.com>
2025-11-25 20:40:56 +0300

    improvements

--- TestApp/Helpers/Extensions.swift
+++ TestApp/Helpers/Extensions.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 extension UIApplication {
-    // Note: deprecated in iOS 13+, kept simple for demo. In production prefer alternative UI.
+    
     var isNetworkActivityIndicatorVisible: Bool {
         get { return false }
         set {  }
